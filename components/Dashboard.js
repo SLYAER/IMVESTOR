fc3c4bf26a3ce648386eba0e5bda7e3be0fe9f7e
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-04-01 17:45:56 +0530

    Update Dashboard.js

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,1 +1,12 @@
+import React from "react";
+import BillCard from "./BillCard";
 
+export default function Dashboard({ bills }) {
+  return (
+    <div className="dashboard">
+      {bills.map((b, i) => (
+        <BillCard key={i} bill={b} />
+      ))}
+    </div>
+  );
+}
